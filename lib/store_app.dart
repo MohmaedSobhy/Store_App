88222862be176384924e7707982dece365ee4eba
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-05-15 17:52:27 +0300

    package env

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/app/env.variables.dart';
 
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: EnvVariables.instanse.getEnvType() == 'Dev',
+      home: const Scaffold(),
     );
   }
 }
